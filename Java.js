444410467bcc70cfd2d2bfd1f628c38701f70372
Lukas-Diffner-Persson <143181744+Lukas-Diffner-Persson@users.noreply.github.com>
2023-12-01 13:18:13 +0100

    håller på med att lägga till en meny
    inte klar en

--- Java.js
+++ Java.js
@@ -15,4 +15,11 @@ function text1() {
     document.getElementById('btn2').style.display = 'none';
   }
 
-  
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
